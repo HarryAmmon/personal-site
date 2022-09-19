ed74d95ca1626c07b24c66d89c396893e330df0f
HarryAmmon <harry_a99@hotmail.co.uk>
2022-09-19 16:05:46 +0100

    Remove copy

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,22 +28,6 @@ export default function Home() {
         </PageWidth>
         <Wave position="top" />
       </SiteWidth>
-
-      {/* Name, Nav bar to navigate to sections on the same page */}
-
-      {/* Desired sections (might be in order, might not be) */}
-      {/* Hire me - button to download my CV in pdf format */}
-      {/* Career history (brief) */}
-      {/* Projects - a short description of projects I've worked on */}
-      {/* Links to socials (github, dev, linkdin, twitter (again maybe)) */}
-      <main className={styles.main}>
-        <div className={styles.card}>
-          <P>Career so far</P>
-        </div>
-        <div className={styles.card}>
-          <P>Projects</P>
-        </div>
-      </main>
     </div>
   );
 }
